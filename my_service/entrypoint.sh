b3790e5d5a0902242d4c452d4b9a9fc8128a24e8
qhauuy <qhauuy@student.42mulhouse.fr>
2025-08-24 13:07:27 +0200

    blabla

--- my_service/entrypoint.sh
+++ my_service/entrypoint.sh
@@ -1,4 +1,5 @@
 
+nginx
 
 if [ ! -f $WORDPRESS_CONTAINER_PATH/wp-config.php ]
 then
@@ -6,7 +7,22 @@ then
 	chown -R www-data:www-data $WORDPRESS_CONTAINER_PATH/wp-content
 fi
 
-nginx
 php-fpm7.4 &
+
+if [ ! -d $DB_CONTAINER_PATH/$DATABASE_NAME ]
+then
+	mariadb-install-db
+	# service mariadb start
+	# mariadb -e "
+	# 	ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_ROOT_PASSWORD';
+	# 	CREATE DATABASE $DATABASE_NAME;
+	# 	CREATE USER '$DATABASE_USER'@'localhost' IDENTIFIED BY '$DATABASE_USER_PASSWORD';
+	# 	GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USER'@'localhost';
+	# 	FLUSH PRIVILEGES;
+	# "
+	# mysqladmin shutdown -p$DATABASE_ROOT_PASSWORD
+fi
+
+mariadbd-safe &
 
 tail -f
