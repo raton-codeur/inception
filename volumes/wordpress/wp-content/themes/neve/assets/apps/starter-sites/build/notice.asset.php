f38234432a085bce10ea13fb64bf9fb6cce7b23e
qhauuy <qhauuy@student.42mulhouse.fr>
2025-08-26 14:34:08 +0200

    blabla

--- volumes/wordpress/wp-content/themes/neve/assets/apps/starter-sites/build/notice.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '32b17eb935c8b128daba658fb0525b6a');
--- volumes/wordpress/wp-content/themes/neve/assets/customizer/js/separator-section.js
+++
@@ -1,9 +0,0 @@
-wp.customize.sectionConstructor.neve_separator = wp.customize.Section.extend({
-	// No events for this type of section.
-	attachEvents() {},
-
-	// Always make the section active.
-	isContextuallyActive() {
-		return true;
-	},
-});
